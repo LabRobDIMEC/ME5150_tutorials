b15b57e8cca4877800a5cdb1fda5de2f66cb3ae4
Karen Quezada <karen.soledad.quezada@gmail.com>
2024-10-12 17:12:12 -0300

    solucion proyecto A

--- tareas/katana_pick_and_place.py
+++ tareas/katana_pick_and_place.py
@@ -15,29 +15,34 @@ p.setGravity(0, 0, -9.81)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("modelos/manipuladores/katana/katana.urdf", basePosition=[0, 0, 0], useFixedBase=useFixedBase)
 football_pitch = p.loadURDF("modelos/manipuladores/katana/football_pitch.urdf", basePosition=[1, 1.08, 0], useFixedBase=True)
-duck2 = p.loadURDF("modelos/manipuladores/katana/duck_orange.urdf", basePosition=[0.28, -0.2, 0],useFixedBase=True)
-duck3 = p.loadURDF("modelos/manipuladores/katana/duck_vhacd.urdf", basePosition=[0.4, 0, 0],useFixedBase=True)
-duck4 = p.loadURDF("modelos/manipuladores/katana/duck_orange.urdf", basePosition=[0.25, 0.18, 0],useFixedBase=True)
+#soccerball = p.loadURDF("modelos/manipuladores/katana/soccerball.urdf", basePosition=[0.19, -0.31, 0.2], useFixedBase=False)
+duck2 = p.loadURDF("modelos/manipuladores/katana/duck_vhacd.urdf", basePosition=[0.3, -0.2, 0],useFixedBase=True)
+duck3 = p.loadURDF("modelos/manipuladores/katana/duck_orange.urdf", basePosition=[0.5, 0.15, 0],useFixedBase=True)
+duck4 = p.loadURDF("modelos/manipuladores/katana/duck_orange.urdf", basePosition=[0.35, 0, 0],useFixedBase=True)
 basket = p.loadURDF("modelos/manipuladores/katana/basket.urdf", basePosition=[0.1, -0.3, 0.15],useFixedBase=True)
 
-# Rotar la canasta
 angle= -0.785398
 orientation= p.getQuaternionFromEuler([angle, 0, 0])
 p.resetBasePositionAndOrientation(basket, [0.1, -0.3, 0.15], orientation)
 
-#TODO: Obtener el número de articulaciones y filtrar las móviles
-#num_joints = 
+
+num_joints = p.getNumJoints(robotId)
 movable_joints = []
 gripper_joints = []
 
-#filtrar las articulaciones móviles 
 for i in range(num_joints):
     joint_info = p.getJointInfo(robotId, i)
     if joint_info[2] == p.JOINT_REVOLUTE or joint_info[2] == p.JOINT_PRISMATIC:
         movable_joints.append(i)
 
+# Parámetros del gripper
+gripper_r_joint_index = 6
+gripper_l_joint_index = 7
+target_position_closed = 0.03  # Quizás cambiar por el radio de la pelota
+target_position_open = 0.2
+max_force = 50
 
-lista_de_obstaculos = [duck2, duck3, duck4] 
+lista_de_obstaculos = [duck2, duck3, duck4] #llenar con el resto de obstaculos  
 basket_obstacle = [basket]  
 grid_size = 0.06 #en terminos de la grilla
 world_size = 0.35 #en terminos de la grilla
@@ -46,36 +51,74 @@ tool_frame_link = 8
 
 
 # Obtener los índices de los links de los dedos del gripper
+gripper_joints = [6, 7]  # Suponiendo que estos son los índices de los dedos del gripper
 grid, obstaculos= grid_and_obstacles(grid_size, world_size, obstacles_height, lista_de_obstaculos)
 
 
+def tool_in_pos_with_nullspace(robotId, endEffectorId, targetPos, threshold, maxIter, lowerLimits, upperLimits, jointRanges, restPoses):
+    closeEnough = False
+    iter = 0
+    dist2 = 1e30
+    
+    while (not closeEnough and iter < maxIter):
+        # Calcular la cinemática inversa con espacio nulo
+        jointPoses = p.calculateInverseKinematics(robotId, endEffectorId, targetPos,
+                                                  lowerLimits=lowerLimits, 
+                                                  upperLimits=upperLimits, 
+                                                  jointRanges=jointRanges, 
+                                                  restPoses=restPoses)
+        p.setJointMotorControlArray(robotId, movable_joints, p.POSITION_CONTROL, targetPositions=jointPoses, forces=[max_force]*len(movable_joints))
+        ls = p.getLinkState(robotId, endEffectorId)
+        newPos = ls[4]  # Posición global del efector final
+        diff = [targetPos[0] - newPos[0], targetPos[1] - newPos[1], targetPos[2] - newPos[2]]
+        dist2 = (diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2])
+        closeEnough = (dist2 < threshold)
+        iter += 1
+    return closeEnough
 
 
-""""definir la función de movimiento que recibe las posiciones a las 
-que debe moverse el robot, entre otros inputs, como robotId o endEffectorId"""
+start_mundo = [0.4, -0.6, obstacles_height]
+goal_mundo = [0.35, 0.33, obstacles_height]
+grip_coordinates= [0.19, -0.31, 0.2]
+start=world_to_grid(start_mundo, grid_size, obstacles_height)
+print("start", start)
+goal=world_to_grid(goal_mundo, grid_size, obstacles_height)
+print("goal", goal)
+# start = (0.48, -0.24, obstacles_height)
+# goal = (0.18, 0.18, obstacles_height)
+
+# for (x,y) in grid:
+#     print("x", x, "y", y)
+#     p.addUserDebugLine([x, y, obstacles_height+0.05], [x+grid_size, y, obstacles_height+0.05], [1, 0, 0], 5)
+#     p.addUserDebugLine([x, y, obstacles_height+0.05], [x, y+grid_size, obstacles_height+0.05], [1, 0, 0], 5)
+
+# # Draw the origin or the first cell in the grid
+# p.addUserDebugLine([0, 0, obstacles_height], [grid_size, 0, obstacles_height], [1, 1, 1], 4)
+# p.addUserDebugLine([0, 0, obstacles_height], [0, grid_size, obstacles_height], [1, 1, 1], 4)
 
 
-# start_mundo = [] #se usan las coordenadas del mundo en 3D, elegir las coordenadas de inicio y goal
-# goal_mundo = []
-# grip_coordinates= [0.19, -0.31, 0.2] 
-# start=world_to_grid(start_mundo, grid_size, obstacles_height) #esta función se puede modificar en picknplace_lib.py
-# goal=world_to_grid(goal_mundo, grid_size, obstacles_height)
-
-"""path es el camino que recorre en la cancha evitando obstáculos"""
 path = a_star_with_obstacles(start, goal, grid, obstaculos, grid_size, obstacles_height)
+print("path", path)
+print("largor del path", len(path))
 
-""" se definen los límites de las articulaciones y los rangos de las articulaciones
-Esta info se encuentra en el archivo katana.urdf"""
 lower_l=[-3.025528, -0.135228, -2.221804, -2.033309, -2.993240]
 upper_l=[2.891097, 2.168572, 2.054223, 1.876133, 2.870985]
 joint_ranges=[5.916625, 2.303800, 4.276027, 3.909442, 5.864225]
 pos_descanso=[0.5, 0.9, -0.5, -1.7]
 
 
-time.sleep(5) # esperar 5 segundos antes de empezar a mover el robot
+time.sleep(5)
 path_index = 0
+#while path_index < len(path):
 while True:
-    #TODO: implementar el movimiento del robot en la simulacion
+    umbral=0.1
+    iteraciones_max=100
+    # Obtener la siguiente posición del camino
+    if path_index < len(path):
+        next_position = path[path_index]
+
+        if tool_in_pos_with_nullspace(robotId, tool_frame_link, next_position, umbral, iteraciones_max, lower_l, upper_l, joint_ranges, pos_descanso):
+            path_index += 1
     
     p.stepSimulation()
-    time.sleep(1 / 240) # se puede ajustar la velocidad de la simulación
+    time.sleep(1 / 240)
--- tareas/picknplace_lib.py
+++ tareas/picknplace_lib.py
@@ -2,22 +2,21 @@ import numpy as np
 from queue import PriorityQueue
 import pybullet as p
 
-def is_in_obstacle(node, obstacle_height, grid_size, lista_de_obstaculos, sub_grid_divisions=10):
-    """Determina si un nodo está dentro de un obstáculo."""
+def is_in_obstacle(node,obstacle_height, grid_size, lista_de_obstaculos, sub_grid_divisions=10):
     sub_grid_size = grid_size / sub_grid_divisions
-    for obstacle_id in lista_de_obstaculos: #recorre los obstaculos dentro de la lista entregada
-        #TODO: encontrar la posición del obstáculo en el mundo.
-        raise NotImplementedError("Falta implementar is_in_obstacle")
+    for obstacle_id in lista_de_obstaculos:
+        aabb_min, aabb_max = p.getAABB(obstacle_id)
         for i in range(sub_grid_divisions):
             for j in range(sub_grid_divisions):
                 sub_node_x = node[0] + i * sub_grid_size
                 sub_node_y = node[1] + j * sub_grid_size
-                #TODO: agregar la condicion para verificar si el nodo está dentro del obstáculo y retornar True
-    return False #si no está dentro de un obstáculo, retorna False
+                if (aabb_min[0] <= sub_node_x <= aabb_max[0] and
+                    aabb_min[1] <= sub_node_y <= aabb_max[1] and
+                    aabb_min[2] <= obstacle_height <= aabb_max[2]):
+                    return True
+    return False
 
 def grid_and_obstacles(grid_size, world_size, obstacles_height, lista_de_obstaculos):
-    """Genera una grilla y una lista de obstáculos en el mundo.
-    Esta función ya está implementada y no es necesario modificarla. """
     grid = []
     obstacles= []
     for x in np.arange(0, world_size*2, grid_size):
@@ -28,28 +27,24 @@ def grid_and_obstacles(grid_size, world_size, obstacles_height, lista_de_obstacu
                 obstacles.append((x,y, obstacles_height))
     return grid, obstacles
 
-
 def reconstruct_path(came_from, current):
-    """ Reconstruye el camino desde el nodo inicial hasta el nodo actual.
-    Esta función ya está implementada y no es necesario modificarla."""
     path = []
     while current in came_from:
         path.append(current)
         current = came_from[current]
-    path.reverse() #invertir el camino para que vaya desde el inicio hasta el final
+    path.reverse()
     return path
 
-
 def get_neighbors(node, grid, obstacles, grid_size, height_z):
-    """" Obtiene los vecinos de un nodo en la grilla.
-    Esta función ya está implementada y no es necesario modificarla."""
-    directions = [(1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, -1, 0)]  # Movimientos en 2D 
+    # Definir las direcciones en términos unitarios, luego escalarlas por el tamaño de la grilla
+    directions = [(1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, -1, 0)]  # Movimientos en 2D y z
     scaled_directions = [(d[0] * grid_size, d[1] * grid_size, d[2] * grid_size) for d in directions]
     
     neighbors = []
     
     for direction in scaled_directions:
         neighbor = (node[0] + direction[0], node[1] + direction[1], height_z + direction[2])
+        #print("Vecino:", neighbor)
         
         is_obstacle = False
         for obs in obstacles:
@@ -61,41 +56,40 @@ def get_neighbors(node, grid, obstacles, grid_size, height_z):
             for cell in grid:
                 if abs(neighbor[0] - cell[0]) < 0.09 and abs(neighbor[1] - cell[1]) < 0.09 and abs(neighbor[2] - height_z) < 0.09:
                     neighbors.append(neighbor)
+                    #print("Celda:", cell)
                     break
 
     return neighbors
 
 def a_star_with_obstacles(start, goal, grid, obstacles, grid_size, obstacles_height):
-    """ aplica el algoritmo de A* para encontrar el camino más 
-    corto entre dos puntos en un entorno con obstáculos."""
-    """" los inputs de esta función podrian modificarse, si se estima necesario"""
     set_abierto = PriorityQueue()
     set_abierto.put((start, 0)) #se pone el elemento en la cola de prioridad
     came_from = {}
     g_score = {start: 0}
-    raise NotImplementedError("Falta implementar a_star_with_obstacles")
     f_score = {start: np.linalg.norm(np.array(start) - np.array(goal))}
     while not set_abierto.empty():
         current,_ = set_abierto.get()
+        #print("current", current)
         if abs(current[0] - goal[0]) < 0.09 and abs(current[1] - goal[1]) < 0.09 and abs(current[2] - goal[2]) < 0.09:
-         """ Si el nodo actual es el nodo objetivo, reconstruir el camino y retornarlo.
-         se recomienta sumar un delta a la coordenada z para que el efecto no choque con la cancha"""
-            
-        
-        """ Recorrer la lista de vecinos previamente calculada 
-        aplicar la formula del algoritmo, (calcular un costo tentativo) con el metodo matematico 
-        de preferencia para la heuristica
-        aplicar una condición para comprobar si el nuevo costo tentativo 
-        es menor que el costo previo registrado
-        en caso de ser menor, se actualiza el costo y se agrega el vecino a la cola de prioridad
-        """
-        set_abierto.put((neighbor, f_score[neighbor]))
+            print("A* llegó")
+            path= reconstruct_path(came_from, current)
+            path_with_delta= [(x,y,z+0.08) for x,y,z in path]
+            return path_with_delta
+        for neighbor in get_neighbors(current, grid, obstacles, grid_size, obstacles_height):
+            tentative_g_score = g_score[current] + np.linalg.norm(np.array(current) - np.array(neighbor))
+            #print("tentative_g_score", tentative_g_score)
+            if tentative_g_score < g_score.get(neighbor, np.inf):
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = g_score[neighbor] + np.linalg.norm(np.array(neighbor) - np.array(goal))
+                set_abierto.put((neighbor, f_score[neighbor]))
     raise ValueError("No se encontró un camino")
 
 def world_to_grid(world_point, grid_size, obstacles_height):
     """
-    convierte un punto del mundo de la simulación a coordenadas de la grilla.
-    De ser necesario, se puede modificar esta función, para obtener mejores resultados en la simulacion
+    Convierte un punto del mundo de la simulación a coordenadas de la grilla.
+    Returns:
+        tuple: Coordenadas en la grilla (x, y).
     """
     x_world, y_world, z_world = world_point
     x_grid = x_world  / 0.1  
