e47f3c4db187d257994650cef4d46d2f38b21a6a
lesliee_ccv <leslie.cardenas008@gmail.com>
2024-10-24 21:21:01 -0300

    base code PB, without comments

---
+++ proyectos/PB/main.py
@@ -0,0 +1,127 @@
+#Importacion de librerias
+import os
+import sys
+import pybullet as p
+import pybullet_data
+import time
+import numpy as np
+import cv2
+from utils import cvK2BulletP, get_img_cam, load_maze
+from info_arucos import *
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../herramientas')))
+from aruco_huntingv2 import ArucoHunting
+from MoveOmni import MoveOmni
+
+# Connect to the PyBullet simulator
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) # Set the search path to find the plane.urdf file
+
+p.setGravity(0,0,-9.81) # Set the gravity to be in the negative z direction
+p.setTimeStep(1 / 240)
+
+planeId = p.loadURDF("plane.urdf") # Load the plane into the simulation
+
+load_maze()
+
+omni_base_pos = [0, 0, 0.08]
+omni_id = p.loadURDF("../modelos/mini_omni/urdf/mini_omni.xacro", basePosition = omni_base_pos, useFixedBase = True)
+
+move_omni = MoveOmni([0, 0, 0], 30)
+
+camera_matrix = np.array([[691.,0. , 289.],[0., 690., 264.], [0., 0., 1.]])
+
+hunter = ArucoHunting()
+hunter.set_marker_length(0.1) # Dado por cubo simulado
+hunter.set_camera_parameters(camera_matrix) # Matriz de la cámara
+
+def update_camera():
+    global img
+    global t_cam
+    global move_omni
+
+    # Cada 0.01 segundos se actualiza la imagen de la cámara
+    if t_cam + 0.01 < time.time():
+        position_omni = move_omni.act_pose
+        theta_rad = position_omni[2]
+        theta_deg = np.rad2deg(theta_rad)
+
+        camorientation = np.array([theta_deg, 0, 0])
+        camposition = np.concatenate([position_omni[:2], [0]]) + [0.05 * np.cos(theta_rad), 0.05 * np.sin(theta_rad), 0.1] # Posición de la cámara respecto al robot
+        p.addUserDebugPoints([camposition], [[1, 0, 0]], pointSize=2, lifeTime=0.001)
+
+        img, _, _ = get_img_cam(width=480,
+                                height=480, 
+                                camposition = camposition, 
+                                camorientation = camorientation, 
+                                cam_mat=camera_matrix)
+        t_cam = time.time()
+    return img
+
+def update_mov():
+    global t_mov
+    global next_pose
+    global move_omni
+
+     # Si no está en la posición objetivo
+    if t_mov + 0.01 < time.time() and move_omni.is_on_target() == False:
+        move_omni.update_pose()
+
+        new_position = np.concatenate([move_omni.act_pose[:2], [0.08]])
+        new_orientation = np.concatenate([ [0.0, 0.0], [move_omni.act_pose[2]]])
+        new_orientation = p.getQuaternionFromEuler(new_orientation)
+
+        p.addUserDebugPoints([new_position], [[0, 1, 0]], pointSize=2, lifeTime=0.001)
+        p.resetBasePositionAndOrientation(omni_id, new_position, new_orientation)
+        t_mov = time.time()
+
+    # Si está en la posición objetivo
+    if move_omni.is_on_target() == True:
+        next_pose = None # Reiniciar la siguiente posición
+
+state = 0         # Variable para controlar que aruco debe buscar
+substate = 0      # Variable para controlar que accion debe hacer
+t_cam = 0         # Variable para controlar la frecuencia de la cámara
+t_mov = 0         # Variable para controlar la frecuencia de movimiento
+img = None        # Imagen de la cámara
+next_pose = None  # Siguiente pose del robot
+target_pose = []
+
+def get_next_pose(img):
+    """Calcular la posición siguiente del robot, basado en la imagen de la cámara
+    Args:
+        img (np.array): imagen de la cámara RGB
+    Returns:
+        next_pose (np.array): [x, y, theta] siguiente posición del robot, en metros y radianes, 
+                    respecto al sistema de coordenadas global.
+    """
+    global state, substate
+    global position_omni   # Posición del robot en metros [x, y]
+    global theta_omni # Orientación del robot en radianes [theta]
+
+    action = [0, 0, 0] # Acción por defecto
+    pose_omni = np.concatenate([position_omni, [theta_omni]])
+
+    # A: Detectar arucos en la imagen
+
+    # B: Con el aruco de interes, calcular la acción a realizar
+
+    # C: Calcular la siguiente posición del robot
+    x0, y0, theta0 = pose_omni # Posición actual del robot
+    x1, y1, theta1 = action # Acción a realizar con respecto a las coordenadas del robot
+
+    next_pose = pose_omni # Modificar 
+    return next_pose
+
+while True:
+    update_camera()
+    position_omni = move_omni.act_pose[:2]
+    theta_omni = move_omni.act_pose[2]
+
+    if next_pose is None:
+        next_pose = get_next_pose(img)
+        if next_pose is not None:
+            move_omni.set_target_pose(next_pose)
+
+    update_mov()
+    p.stepSimulation()
---
+++ proyectos/PB/utils.py
@@ -0,0 +1,46 @@
+
+import pybullet as p
+import numpy as np
+import cv2
+from info_arucos import *
+
+# No modificar - Calculo de matriz de proyeccion de camara
+def cvK2BulletP(width, height, cam_mat, near, far):
+    fx, fy, cx, cy = cam_mat[0, 0], cam_mat[1, 1], cam_mat[0, 2], cam_mat[1, 2]
+    aspect = width / height
+    fov = 2 * np.arctan(height / (2 * fy)) * 180 / np.pi
+    projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
+    return projection_matrix
+
+# No modificar - Obtener imagen de camara simulada
+def get_img_cam(width=480,
+                height=480, 
+                near=0.1, 
+                far=100, 
+                camposition=[0, 0, 1.5], 
+                camorientation = [0, 0, 0], 
+                distance=0.1, 
+                cam_mat = None):
+
+    aspect = width / height
+    yaw, pitch, roll = camorientation + [-90, 0, 0]
+    view_matrix = p.computeViewMatrixFromYawPitchRoll(camposition, distance, yaw, pitch, roll, upAxisIndex = 2)
+    projection_matrix = cvK2BulletP(width, height, cam_mat, near, far)
+    _, _, rgbaImg, depthImg, segImg = p.getCameraImage(width, height, view_matrix, projection_matrix, renderer=p.ER_BULLET_HARDWARE_OPENGL,flags = p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX)
+    rgbaImg = cv2.cvtColor(rgbaImg, cv2.COLOR_BGR2RGB)
+    return rgbaImg, segImg, depthImg
+
+def load_maze():
+    pos_laberinto = [0.5, 0.75, 0]
+    or_laberinto = p.getQuaternionFromEuler([0, 0, np.pi/2])
+    p.loadURDF('../modelos/entornos/laberinto/laberinto.urdf', basePosition = pos_laberinto, baseOrientation = or_laberinto, useFixedBase = True)
+    laberinto_origin = np.array([0.5, 0, 0])
+
+    for aruco in info_arucos["arucos_l1"]:
+        aruco_info = info_arucos["arucos_l1"][aruco]
+        pos = np.array([float(aruco_info["x"]), float(aruco_info["y"]), 0.08]) + laberinto_origin
+        orien = p.getQuaternionFromEuler([0, np.pi/2, np.deg2rad(float(aruco_info["rz"]))])
+        id = aruco_info["id"]
+        p.loadURDF(f"../modelos/objetos/Arucos/A{id}/A{id}.urdf", basePosition = pos, baseOrientation = orien, useFixedBase = True)
+        print(f"ID: {aruco_info['id']}, Pos: {pos}")
+
